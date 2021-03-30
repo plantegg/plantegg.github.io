3669d98b23d7e0c56b130e48eeaac842a8fd6650
xijun.rxj <xijun.rxj@alibaba-inc.com>
2021-03-30 15:00:46 +0800

    add intranetproxy.alipy.com to img.sh

--- img.sh
+++ img.sh
@@ -1,10 +1,10 @@
 rm -f ./ossimg/*
 
-grep -E "cn-hangzhou.oss-pub.aliyun-inc.com|img.oss-cn-zhangjiakou.aliyuncs.com" source/_posts/*.md  | grep "img src=" | awk -F '"' '{ print $2 }' >img.list
+grep -E "intranetproxy.alipay.com|cn-hangzhou.oss-pub.aliyun-inc.com|img.oss-cn-zhangjiakou.aliyuncs.com" source/_posts/*.md  | grep "img src=" | awk -F '"' '{ print $2 }' >img.list
 
-grep -E "cn-hangzhou.oss-pub.aliyun-inc.com|img.oss-cn-zhangjiakou.aliyuncs.com" source/_posts/*.md  | grep -v "img src=" | awk -F "(" '{ print $NF '} | sed 's/)//g' >>img.list
+grep -E "intranetproxy.alipay.com|cn-hangzhou.oss-pub.aliyun-inc.com|img.oss-cn-zhangjiakou.aliyuncs.com" source/_posts/*.md  | grep -v "img src=" | awk -F "(" '{ print $NF }' | sed 's/)//g' >>img.list
 
-dos2unix img.list
+#dos2unix img.list
 cat img.list | sort | uniq >img_uniq.list
 while read line; do echo $line ; wget -q $line -P ./ossimg/ ; done <./img_uniq.list
 
